3c29d91d962265f1d0322eb646a4490a064e9a9a
LorenzoM <l.merli@generalfinance.it>
2020-04-03 14:52:55 +0200

    NEW - VOCI BILANCIO MASTER

--- plan/src/app/app-routing.module.ts
+++ plan/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VocibilancioComponent } from './vocibilancio/vocibilancio.component';
+import { VocibilanciomasterComponent } from './vocibilanciomaster/vocibilanciomaster.component';
 
 const routes: Routes = [
-{path: 'vocibilancio', component:VocibilancioComponent}
-]
-;
+{path: 'vocibilancio', component: VocibilancioComponent},
+{path: 'vocibilanciomaster', component: VocibilanciomasterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
